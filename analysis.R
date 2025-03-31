4d92d75b837b6af93adbca9290483e6839064c03
Necmi Gunduz <necmigunduz@gmail.com>
2025-03-31 14:22:53 +0300

    Checking accuracy of different prediction models

--- analysis.R
+++ analysis.R
@@ -1,9 +1,1 @@
-library(tidyverse)
-library(ggplot2)
-load("rda/murders.rda")
-
-murders %>% mutate(abb = reorder(abb, rate)) %>% 
-  ggplot(aes(abb, rate)) +
-  geom_bar(width = 0.5, stat = "identity", color = "black") +
-  coord_flip()
-ggsave("figs/barplot.png")
+The system cannot find the file specified.
